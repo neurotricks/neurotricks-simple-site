6d489740763fd4e85a46634f2b4aa7b423aafd40
Neurotricks <218985744+neurotricks@users.noreply.github.com>
2025-07-03 21:38:48 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
   site: 'https://neurotricks.com',
+  integrations: [tailwind()],
+  styles: ['src/styles/global.css'],
 });
